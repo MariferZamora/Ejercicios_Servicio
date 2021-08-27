beec1ddabb4ba8b4d59301c0de423e47b36df502
MariferZamora <marifer9509@hotmail.com>
2021-08-27 02:21:55 -0500

    conectando axios con PokeApi

--- pokemon/src/views/About.vue
+++ pokemon/src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{pokemon.name}}</h1>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data:()=>({
+    pokemon: null
+  }),
+
+  created(){
+    this.getPokemon()
+  },
+
+  methods:{
+
+    getPokemon(){
+      const axiosInstance=axios.create({
+       headers:{ 
+         "Access-Control-Allow-Origin": "*"
+        }
+      });
+      axiosInstance
+      .get(`https://pokeapi.co/api/v2/pokemon/1`)
+      .then(response=>{
+        this.pokemon=response.data
+        console.log(this.pokemon)
+        const{name, base_experience}=response.data
+        console.log(name)
+        console.log(base_experience)
+        const habilidades=response.data.abilities
+        console.log(habilidades)
+        const habilidad= response.data.abilities[1].ability.name
+        console.log(habilidad)
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
