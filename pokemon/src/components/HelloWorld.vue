7ebe4313dddeb17c70f411c31282eb90e5b30468
MariferZamora <marifer9509@hotmail.com>
2021-08-28 01:16:20 -0500

    galeria de imagenes
    creando una galería de imágenes de libre elección con axios, con key de Api en GIPHY

--- pokemon/src/components/HelloWorld.vue
+++ pokemon/src/components/HelloWorld.vue
@@ -1,62 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="min-h-screen flex items-center justify-center">
+      <h1>Galeria de Imagenes de los Simpson</h1>
+      <div>
+        <img V-for="(imagen, i) in galeria" :key="i" :src="imagen.data.images.downsized.url" :alt="imagen.data.title">
+      </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      galeria:[]
+    }
+
+  },
+
+  created(){
+    this.getPokemon()
+  },
+
+  methods:{
+    async getPokemon(){
+      const response=await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jUXq6uENWLkSViX2W9NacxL21RJbD5gZ&q=Simpson&limit=25&offset=0&rating=g&lang=es`)
+      this.galeria=response.data
+      console.log(this.galeria.data[0].images.downsized.url)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
 </style>
--- pokemon/src/views/Home.vue
+++ pokemon/src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home min-h-screen flex items-center justify-center">
+    <div>
+        <h1 class="text-4xl leading-6 p-6 font-medium text-gray-900 ">
+          Galeria de Imagenes de los Simpson
+          </h1>
+        <img class="h-70 w-70 rounded-full p-4" v-for="imagen in imagenes" :key="imagen.id" :src="imagen" alt="Simpson">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      galeria:[],
+      imagenes:[],
+      titulos:[]
+    }
+
+  },
+
+  created(){
+    this.getPokemon()
+  },
+
+  methods:{
+    async getPokemon(){
+      const response=await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jUXq6uENWLkSViX2W9NacxL21RJbD5gZ&q=Simpson&limit=30&offset=0&rating=g&lang=es`)
+      this.galeria=response.data
+      for(let i=0; i<30; i++){
+        this.imagenes[i]=this.galeria.data[i].images.downsized.url
+      }
+      //console.log(this.imagenes)
+      //console.log(this.galeria.data[0].images.downsized.url)
+    }
   }
 }
 </script>
